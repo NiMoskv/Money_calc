5774e1377a8ded2cfa81c42789a5575a0197dc75
Nikita <nvm96@yandex.ru>
2023-02-13 22:03:06 +0300

    Third commit

--- Sprint2_Final.py
+++ Sprint2_Final.py
@@ -23,56 +23,74 @@ class Calculator:
         self.records = []
   
     def add_record(self, record):
-        print(self.record)
+        self.records.append(record)
+        print(self.records, 'It is records')
     
     def get_today_stats(self):
-        print(self.records)
-
-
+        for item in self.records:
+            if item.date == dt.datetime.now().date():
+                amount_sum += item.amount
         
+        return amount_sum
+
+    def get_week_stats(self):
+        for day in range (len(self.records)-1, len(self.records)-8):
+            summary_value += day.amount
         
+        return summary_value
 
 
-# class CaloriesCalculator(Calculator):
-#     """
-#     This is a calorie calculator. Module can keep a record of meals, Count
-#     calories eaten,determine the calories left for today, sum up the calories
-#     for the last week.
-#     """
+class CaloriesCalculator(Calculator):
+    """///"""
+    def __init__(self, limit: int) -> None:
+        super().__init__(limit)
+
+    def get_calories_remained(self):
+        if self.get_today_stats()<self.limit:
+            return f'Сегодня можно съесть что-нибудь еще, но с общей калорийностью не более {self.limit - self.get_today_stats()} кКал'
+        
+        return 'Хватит есть!'
+
+
+class CashCalculator:
+    """"""
+    def __init__(self, limit) -> None:
+        super().__init__(limit)
     
-#     def __init__(self, limit) -> None:
-#         super().__init__(limit)
-       
-        
-        
-#     def get_today_stats():
-#         pass
-#     def get_calories_remained():
-#         pass
-#     def get_week_stats():
-#         pass
+    def get_today_cash_remainder(self, currency:str = 'rub'):
+        remaining_money = self.limit - self.get_today_stats()
+        usd=73
+        eur=78
+        if currency =='usd':
+            remaining_money = round((remaining_money / usd),2)
+        elif currency == 'eur':
+            remaining_money = round((remaining_money / eur),2)
 
-r1 = Record(amount=691, comment='Катание на такси', date='08.03.2019')
-r=Calculator(1000)
-f=Calculator.add_record(691)
+        if self.get_today_stats()<self.limit:
+            return f'На сегодня осталось {remaining_money} {currency}'
+        elif self.get_today_stats() == self.limit:
+            return 'Денег нет, держись брат'
+        else:
+            return f'Денег нет, держись: Твой долг {remaining_money}{currency}'
 
-# class CashCalculator:
-#     ...
+            
 
-# # создадим калькулятор денег с дневным лимитом 1000
-# cash_calculator = CashCalculator(1000)
 
-# # дата в параметрах не указана,
-# # так что по умолчанию к записи
-# # должна автоматически добавиться сегодняшняя дата
-# cash_calculator.add_record(Record(amount=145, comment='кофе'))
-# # и к этой записи тоже дата должна добавиться автоматически
-# cash_calculator.add_record(Record(amount=300, comment='Серёге за обед'))
-# # а тут пользователь указал дату, сохраняем её
-# cash_calculator.add_record(Record(amount=3000,
-#                                   comment='бар в Танин др',
-#                                   date='08.11.2019'))
 
-# print(cash_calculator.get_today_cash_remained('rub'))
+# создадим калькулятор денег с дневным лимитом 1000
+cash_calculator = CashCalculator(1000)
+
+# дата в параметрах не указана,
+# так что по умолчанию к записи
+# должна автоматически добавиться сегодняшняя дата
+cash_calculator.add_record(Record(amount=145, comment='кофе'))
+# и к этой записи тоже дата должна добавиться автоматически
+cash_calculator.add_record(Record(amount=300, comment='Серёге за обед'))
+# а тут пользователь указал дату, сохраняем её
+cash_calculator.add_record(Record(amount=3000,
+                                  comment='бар в Танин др',
+                                  date='08.11.2019'))
+
+print(cash_calculator.get_today_cash_remained('rub'))
 # # должно напечататься
 # # На сегодня осталось 555 руб 
